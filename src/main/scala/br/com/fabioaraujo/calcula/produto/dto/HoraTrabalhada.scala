320b8bebccd4ed9c85240d0ff30d87c4aef0ad4c
Fabio Araujo <fabioaraujods@gmail.com>
2017-11-29 22:31:41 -0200

    alterado modelo para inclui N itens para confecção criada entidade de hora trabalhada

---
+++ src/main/scala/br/com/fabioaraujo/calcula/produto/dto/HoraTrabalhada.scala
@@ -0,0 +1,6 @@
+package br.com.fabioaraujo.calcula.produto.dto
+
+import scala.beans.BeanProperty
+
+case class HoraTrabalhada(@BeanProperty val custo : Double) {
+}
--- src/main/scala/br/com/fabioaraujo/calcula/produto/dto/Item.scala
+++ src/main/scala/br/com/fabioaraujo/calcula/produto/dto/Item.scala
@@ -1,10 +1,13 @@
 package br.com.fabioaraujo.calcula.produto.dto
 
+import org.springframework.beans.CachedIntrospectionResults
+
 import scala.beans.BeanProperty
 
 class Item extends Serializable  {
   @BeanProperty var id : Long = _
   @BeanProperty var nome : String = _
   @BeanProperty var custo : Double = _
-  @BeanProperty var horaTrabalhada : Item = _
+  @BeanProperty var horasTrabalhadas : Int = _
+  @BeanProperty var itens : List[Item] = _
 }
