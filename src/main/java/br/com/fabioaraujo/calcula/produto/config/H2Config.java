f23622b3efce465ebef07ce624481d78a443d4b0
Fabio Araujo <fabioaraujods@gmail.com>
2020-07-18 01:05:28 -0300

    obtendo url do banco das variaveis de ambiente

--- src/main/java/br/com/fabioaraujo/calcula/produto/config/H2Config.java
+++ src/main/java/br/com/fabioaraujo/calcula/produto/config/H2Config.java
@@ -4,7 +4,9 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("dev")
 @Configuration
 public class H2Config {
 
